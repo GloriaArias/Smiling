cb444efb1b47dd50f1a0259fe3235a442aa91f47
GloriaArias <ariasutreragloria@gmail.com>
2022-06-08 11:57:40 -0500

    Boiler Plate

---
+++ routes/about.js
@@ -0,0 +1,21 @@
+// Importamos Express
+var express = require("express");
+// Importamos el enrutador de Express
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  //render manda a renderizar (generar y entregar)
+  res.render(
+    "about",
+    // Este es el View_Model
+
+    {
+      name: "Gloria Arias Utrera",
+      email: "ariasutreragloria@gmail.com",
+      url: "www.itgam.com/gloria.arias",
+    }
+  );
+});
+
+module.exports = router;
