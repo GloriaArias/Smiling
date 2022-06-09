61b976bc3f6b42b4ed731822fe273faebce83a17
GloriaArias <ariasutreragloria@gmail.com>
2022-06-08 20:32:51 -0500

    Adding Babel-watch

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,14 +2,14 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  //Render manda a renderizar (generar y entregar) ls vista al cliente
-  let emojieDataset = ["💻", "👨‍💻", "🎈", "🎄", "🦺", "🚵‍♀️", "🎁"];
+router.get('/', function (req, res) {
+  // render manda a renderizar (generar y entregar)
+  // la vista al cliente
+  // Calculando
+  let emojieDataset = ['💻', '👨‍💻', '🎈', '🎄','🦺','🎢','🎁','🚆','🌍','♥'];
   let emojie = emojieDataset[Math.floor(Math.random() * emojieDataset.length)];
-
-  res.render(
-    "index",
-    //Este es el View-Model
+  res.render('index',
+    // Este es el View-Model
     {
       title: "Express",
       author: "Gloria Arias",
