4047d1567e293f0ff36690929d4bdf7a35b92b46
GloriaArias <ariasutreragloria@gmail.com>
2022-06-12 22:46:13 -0500

    S48 Creando proyectos en DB

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,4 +1,7 @@
 import log from '../config/winston';
+// Importando el modelo Project
+import ProjectModel from '../models/ProjectModel';
+
 /* Actions Methods */
 
 // Citas Pendientes
@@ -23,7 +26,7 @@ const addPost = (req, res) => {
   let project = {};
   let errorModel = {};
   if (errorData) {
-    log.info('Se retorna objeto de error de validacion');
+    log.error('🚫 Se retorna objeto de error de validacion');
     // Rescantado el objeto validado
     project = errorData.value;
     // Usamos reduce para generar un objeto
@@ -40,18 +43,30 @@ const addPost = (req, res) => {
     // La validacion fallo
     // res.status(200).json(errorData);
   } else {
-    log.info('Se retorna objeto valido');
+    log.info('✅ Se retorna objeto valido');
     // Desestructurando la informacion
     // del formulario del objeto valido
     const { validData } = req;
-    // Regresar un objeto con los datos
-    // obtenidos del formulario
-    // res.status(200).json(validData);
-    project = validData;
+    // Crear un documento con los datos provistos
+    // por el formulario y guardar dicho documento
+    // en projectModel
+    const projectModel = new ProjectModel(validData);
+    // Siempre que se ejecuta una operacion
+    // que depende de un tercero, es una buena practica
+    // envolver esa operacion en un bloque try
+    try {
+      // Se salva el documento projecto
+      log.info('Se salva objeto Projecto');
+      project = await projectModel.save();
+    } catch (error) {
+      log.error(`Ha fallado el intento de salvar un proyecto:${error.message}`);
+      return res.status(500).json({ error });
+    }
   }
   // Respondemos con los viewModels generados
-  res.render('projects/addProjectView', { project, errorModel });
+  //res.render('projects/addProjectView', { project, errorModel });
   // res.status(200).json({ project, errorModel });
+  return res.status(200).json({ project, errorModel });
 };
 
 // Exportando el controlador
